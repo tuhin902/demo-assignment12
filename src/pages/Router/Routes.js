2ff2cb90c37538184fda244f01806c7afe7cfac1
tuhin902 <tuhin902md@gmail.com>
2022-11-10 00:01:55 +0600

    first commit

---
+++ src/pages/Router/Routes.js
@@ -0,0 +1,20 @@
+import Main from "../../Layout/Main";
+import Home from "../Shared/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+]);
+
+export default router;
+
